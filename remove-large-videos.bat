c7e850681c4f5c57ac44b4d86eab4518c3ae3e3b
Djordje Stankovic <djordje.stankovic992@gmail.com>
2026-01-28 12:06:26 +0100

    Initial commit - Skaska site (compressed videos)

---
+++ remove-large-videos.bat
@@ -0,0 +1,3 @@
+@echo off
+git rm --cached --ignore-unmatch "skaska/videos/Izjava Marko.m4v" "skaska/videos/Izjava Zoran.m4v" "skaska/videos/Izjave komplet final .mp4"
+exit 0
---
+++ remove-large-videos.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git rm --cached --ignore-unmatch "skaska/videos/Izjava Marko.m4v" "skaska/videos/Izjava Zoran.m4v" "skaska/videos/Izjave komplet final .mp4" || true
