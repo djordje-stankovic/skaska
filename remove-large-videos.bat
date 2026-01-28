3a8e390a1a097edebb5afa4312272857af083e35
Djordje Stankovic <djordje.stankovic992@gmail.com>
2026-01-28 12:59:27 +0100

    Move site to docs folder for GitHub Pages

--- remove-large-videos.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-git rm --cached --ignore-unmatch "skaska/videos/Izjava Marko.m4v" "skaska/videos/Izjava Zoran.m4v" "skaska/videos/Izjave komplet final .mp4"
-exit 0
--- remove-large-videos.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git rm --cached --ignore-unmatch "skaska/videos/Izjava Marko.m4v" "skaska/videos/Izjava Zoran.m4v" "skaska/videos/Izjave komplet final .mp4" || true
